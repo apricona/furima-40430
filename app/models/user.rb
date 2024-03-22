6b45b7950ebe335bca2234869e8cd89334c665eb
apricona <anzu.sd.42@gmail.com>
2024-03-22 22:32:46 +0900

    モデルにてバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :date_of_birth, presence: true
+  validates :last_name, :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は全角漢字、かな、カナのみ使用できます" }
+  validates :last_name_kana, :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: "は全角漢字、かな、カナのみ使用できます" }
+
+
+  validates :nickname, :date_of_birth, presence: true
 
   has_many :items
   has_many :orders
