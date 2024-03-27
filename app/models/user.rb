9a259b27d5fd7e22aa0d32eb012a773fc11205d2
apricona <anzu.sd.42@gmail.com>
2024-03-27 13:48:33 +0900

    Userモデル単体テストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :last_name, :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角漢字、かな、カナで
-  ご入力ください' }
+  validates :last_name, :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角漢字、かな、カナでご入力ください' }
   validates :last_name_kana, :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カナでご入力ください' }
 
   validates :nickname, :date_of_birth, presence: true
 
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}\z/i }
+
+
   has_many :items
   has_many :orders
 end
+
