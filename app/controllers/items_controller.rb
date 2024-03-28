36f6a36bc6cfa8ff76112e7c778d50a05bfe2a48
apricona <anzu.sd.42@gmail.com>
2024-03-28 13:05:37 +0900

    ルーティング、コントローラー設定完了

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,30 @@
 class ItemsController < ApplicationController
 
   def index
+    @item = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+    redirect_to'/'
+  end
+
+  private
+  def item_params
+    params.require(:item).parmit(
+      :item_name,
+      :item_description,
+      :category_id,
+      :item_condition_id,
+      :shipping_fee_id,
+      :prefecture_id,
+      :days_to_ship_id,
+      :price,
+      :user,
+      )
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,16 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+
+  validates :item_name,
+            :item_description,
+            :category_id,
+            :item_condition_id,
+            :shipping_fee_id,
+            :prefecture_id,
+            :days_to_ship_id,
+            :price,
+            :user,
+            presence: true
 end
