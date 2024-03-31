f530feb409528d1181dede7d695da3e325daa3ca
apricona <anzu.sd.42@gmail.com>
2024-04-01 03:46:22 +0900

    JSファイルの作成設定完了

--- app/models/item_condition.rb
+++ app/models/item_condition.rb
@@ -3,7 +3,7 @@ class ItemCondition < ActiveHash::Base
   self.data = [
     { id: 1, name:'---'},
     { id: 2, name:'新品'},
-    { id: 3, name:'使用'},
+    { id: 3, name:'未使用'},
     { id: 4, name:'未使用に近い'},
     { id: 5, name:'目立った傷や汚れなし'},
     { id: 6, name:'やや傷や汚れあり'},
