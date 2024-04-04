5a105198d338647b9fc9e6be6ab97b50cea5d131
apricona <anzu.sd.42@gmail.com>
2024-04-04 18:12:58 +0900

    デプロイのための修正②

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -40,7 +40,7 @@ class ItemsController < ApplicationController
       @item.destroy
       redirect_to root_path
     else
-      redirect_to root_path,
+      redirect_to root_path
     end
   end
 
