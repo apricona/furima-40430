410d6b07aa808461ac1697f6ab6f581e7484c9df
apricona <anzu.sd.42@gmail.com>
2024-04-07 14:33:28 +0900

    決済機能のクライアントサイド実装

---
+++ app/javascript/card.js
@@ -0,0 +1,24 @@
+const pay = () => {
+  const payjp = Payjp('pk_test_13c8a0a71e0d173cba5d307c')
+  const elements = payjp.elements();
+  const numberElement = elements.create('cardNumber');
+  const expiryElement = elements.create('cardExpiry');
+  const cvcElement = elements.create('cardCvc');
+  numberElement.mount('#number-form');
+  expiryElement.mount('#expiry-form');
+  cvcElement.mount('#cvc-form');
+
+  const form = document.getElementById('charge-form')
+  form.addEventListener("submit", (e) =>{
+    payjp.createToken(numberElement).then(function (response) {
+      if (response.error) {
+      } else {
+        const token = response.id;
+        console.log(token)
+      }
+    });
+    e.preventDefault();
+  });
+};
+
+window.addEventListener("load", pay);
